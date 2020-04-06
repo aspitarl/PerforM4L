1e218aa473bd42097ed5050e42bccff58cc48caf
aspitarl <aspitarte@gmail.com>
2020-04-05 18:44:52 -0700

    separate out range set mode into function
    removed exit functionality

--- VR/controller_midi.py
+++ VR/controller_midi.py
@@ -18,7 +18,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     controller_name = "controller_" + str(sys.argv[1])
 
-
+print("connecting to " + controller_name)
 contr = v.devices[controller_name]
 
 if controller_name == "controller_1":
@@ -39,7 +39,7 @@ elif controller_name == "controller_2":
 
     midiportname = 'Controller B'
 
-print(controller_name)
+
 
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
@@ -127,7 +127,76 @@ def scale_data_half(data_raw, cube_ranges, dim):
 
     return scaled
 
-# debug = True
+def range_set_mode(contr, debugstr=''):
+    data = contr.get_pose_euler()
+
+    while(data is None):
+        debugstr = debugstr + '\nGot None for data, trying again'
+        time.sleep(0.01)
+        data = contr.get_pose_euler()
+
+
+    x, y, z = extractxyz(data)
+
+    cube_ranges = {
+        'x': {
+            'min': x,
+            'max': x
+        },
+        'y': {
+            'min': y,
+            'max': y
+        },
+        'z':  {
+            'min': z,
+            'max': z
+        }
+    }      
+
+
+    rangesetbutton = 2
+    while(rangesetbutton==2):
+        debug = True  
+
+        inputs = contr.get_controller_inputs()
+
+        rangesetbutton = inputs['ulButtonPressed']
+
+        if debug: debugstr = 'Range Set Mode: '
+        if debug: debugstr = debugstr + '\nPose: ' + str(data)
+
+        data = contr.get_pose_euler()
+
+        if data is not None:
+            x, y, z = extractxyz(data)
+            data_raw = {'x': x, 'y': y, 'z': z}
+
+            for dim in data_raw:
+                if data_raw[dim] < cube_ranges[dim]['min']:
+                    cube_ranges[dim]['min'] = data_raw[dim]
+                elif data_raw[dim] > cube_ranges[dim]['max']:
+                    cube_ranges[dim]['max'] = data_raw[dim]
+
+            if debug: debugstr = debugstr + '\nRange: ' + str(cube_ranges)
+        
+        #Old code for exiting
+        # trigger = inputs['trigger']
+        # if  trigger == 1:
+        #     #exit button (have to fully pull trigger)
+        #     running = False
+        #     rangesetbutton = False
+
+        sleep_time = interval-(time.time()-start)
+        if sleep_time>0:
+            time.sleep(sleep_time)
+        
+        if debug:
+            #not working in anaconda prompt?
+            os.system('cls')
+            print(debugstr)
+
+    return cube_ranges
+    
 
 running = True
 while(running):
@@ -143,72 +212,7 @@ while(running):
 
     if rangesetbutton == 2:
         #enter range set mode
-           
-
-        data = contr.get_pose_euler()
-
-        while(data is None):
-            debugstr = debugstr + '\nGot None for data, trying again'
-            time.sleep(0.01)
-            data = contr.get_pose_euler()
-
-
-        x, y, z = extractxyz(data)
-
-        cube_ranges = {
-            'x': {
-                'min': x,
-                'max': x
-            },
-            'y': {
-                'min': y,
-                'max': y
-            },
-            'z':  {
-                'min': z,
-                'max': z
-            }
-        }      
-
-        while(rangesetbutton==2):
-            debug = True  
-            rangesetbutton = inputs['ulButtonPressed']
-
-            if debug: debugstr = 'Range Set Mode: '
-            if debug: debugstr = debugstr + '\nPose: ' + str(data)
-
-            data = contr.get_pose_euler()
-
-            if data is not None:
-                x, y, z = extractxyz(data)
-                data_raw = {'x': x, 'y': y, 'z': z}
-
-                for dim in data_raw:
-                    if data_raw[dim] < cube_ranges[dim]['min']:
-                        cube_ranges[dim]['min'] = data_raw[dim]
-                    elif data_raw[dim] > cube_ranges[dim]['max']:
-                        cube_ranges[dim]['max'] = data_raw[dim]
-
-                if debug: debugstr = debugstr + '\nRange: ' + str(cube_ranges)
-            
-            trigger = contr.get_controller_inputs()['trigger']
-
-            if  trigger == 1:
-                #exit button (have to fully pull trigger)
-                running = False
-                rangesetbutton = False
-
-            inputs = contr.get_controller_inputs()
-
-            sleep_time = interval-(time.time()-start)
-            if sleep_time>0:
-                time.sleep(sleep_time)
-            
-            if debug:
-                #not working in anaconda prompt?
-                os.system('cls')
-                print(debugstr)
-                
+        cube_ranges = range_set_mode(contr, debugstr)
     else:
         #normal mode
         if debug: debugstr = debugstr + '\nNormal Mode:'
